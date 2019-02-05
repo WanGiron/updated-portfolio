ac143def458a238735beb9940bff65f4c3439e26
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-02-05 17:55:39 -0500

    version1.1

---
+++ assets/script/main.js
@@ -0,0 +1,5 @@
+alert("page under construction!!! some minor details are missing")
+var coming = "coming soon!!!";
+$("#about, #message, #projects, #something ").on('click', function(event){
+    alert(coming);
+})
